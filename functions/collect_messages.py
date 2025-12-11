ac4dd8e420feac9459348641a7649b34130a3a22
Redume <danilivasenko09@gmail.com>
2025-12-11 16:25:18 +0300

    chore: filtered and del all services message

--- functions/collect_messages.py
+++ functions/collect_messages.py
@@ -1,23 +1,35 @@
 import itertools
 
 def collect_messages(data: dict) -> list[dict]:
-
     chats_list = data.get("chats", {}).get("list")
     left_chats_list = data.get("left_chats", {}).get("list")
     
+    source_lists = (l for l in (chats_list, left_chats_list) if l)
+    
     if chats_list or left_chats_list:
         all_messages = []
-        source_lists = (l for l in (chats_list, left_chats_list) if l)
-        
-        for chat_list in source_lists:
-            for chat in chat_list:
-                msgs = chat.get("messages")
-                if msgs and isinstance(msgs, list):
-                    all_messages.extend(msgs)
+        all_chats = itertools.chain.from_iterable(source_lists)
+
+        for chat in all_chats:
+            msgs = chat.get("messages")
+            
+            if msgs and isinstance(msgs, list):
+
+                filtered_msgs = [
+                    msg for msg in msgs 
+                    if isinstance(msg, dict) and msg.get("type") != "service"
+                ]
+                all_messages.extend(filtered_msgs)
+                
         return all_messages
 
     root_msgs = data.get("messages")
+    
     if root_msgs and isinstance(root_msgs, list):
-        return root_msgs
+        filtered_root_msgs = [
+            msg for msg in root_msgs 
+            if isinstance(msg, dict) and msg.get("type") != "service"
+        ]
+        return filtered_root_msgs
 
     return []
